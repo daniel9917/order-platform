6e8069387767829d86e3d504a728fb55fbf5c387
daniel.jimenezm <daniel.jose.45@hotmail.com>
2023-09-07 11:20:45 -0500

    Adding generic kafka module

---
+++ infrastructure/kafka/kafka-producer/src/main/java/com/ordering/platform/kafka/producer/exception/KafkaProducerException.java
@@ -0,0 +1,8 @@
+package com.ordering.platform.kafka.producer.exception;
+
+
+public class KafkaProducerException extends RuntimeException{
+    public KafkaProducerException(String message) {
+        super(message);
+    }
+}
